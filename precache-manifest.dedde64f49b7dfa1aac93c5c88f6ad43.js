6b33e4c4e7e7d5ce053cba74ac0c4ccf8a1f6648
Utkarsh Yadav <yadavutkarsh207@gmail.com>
2020-04-15 23:56:48 +0530

    Updates

---
+++ precache-manifest.dedde64f49b7dfa1aac93c5c88f6ad43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91944eb1dd4710019b287bf43a9a101",
+    "url": "/weatherly/index.html"
+  },
+  {
+    "revision": "a60d4f65a5d36cf2c3cd",
+    "url": "/weatherly/static/css/main.5da743b2.chunk.css"
+  },
+  {
+    "revision": "927eec2e12fda1aaae7f",
+    "url": "/weatherly/static/js/2.632eb01b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherly/static/js/2.632eb01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60d4f65a5d36cf2c3cd",
+    "url": "/weatherly/static/js/main.42058468.chunk.js"
+  },
+  {
+    "revision": "b3c01a1964de9075a598",
+    "url": "/weatherly/static/js/runtime-main.87d9ad3e.js"
+  },
+  {
+    "revision": "83b2ba1f6c0e4b70ed8295278a7d288f",
+    "url": "/weatherly/static/media/milada-vigerova-uiYPlABvWBM-unsplash.83b2ba1f.jpg"
+  },
+  {
+    "revision": "d3d11133a00c0ced8c9ed2ee3f179341",
+    "url": "/weatherly/static/media/nattu-adnan-atSUvc1hMwk-unsplash.d3d11133.jpg"
+  }
+]);
